e915479612a402de2a4b663ee2914a7c2b79250f
Dustin Krysak <dustin@bashfulrobot.com>
2022-07-06 17:12:08 -0700

    refactor: :recycle: trigger build

--- hello-world.go
+++ hello-world.go
@@ -14,80 +14,73 @@ type Context struct {
 	Links [2]string
 }
 
-// Data for HTML templates
-// type MyData struct {
-// 	Title string
-
-// 	Homelinktxt string
-// 	Homelink    string
-
-// 	Hellolinktxt string
-// 	Hellolink    string
-
-// 	Headerlinktxt string
-// 	Headerlink    string
-// }
-
-func hello(w http.ResponseWriter, req *http.Request) {
-
+func writeHtmlHeader(w http.ResponseWriter, req *http.Request, context Context) {
 	const header = `
-<!DOCTYPE html>
-<html>
-    <head>
-		<title>
-        {{.Title}}
-		</title>
-    </head>
-	<style>
-			nav {
-				background: #515151;
-				text-align: center;
-				font-family: Arial, Helvetica, sans-serif;
-				/* THIS IS OPTIONAL*/
-			}
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<title>
+			{{.Title}}
+			</title>
+		</head>
+		<style>
+				nav {
+					background: #515151;
+					text-align: center;
+					font-family: Arial, Helvetica, sans-serif;
+					/* THIS IS OPTIONAL*/
+				}
 
-			h1, p {
-				font-family: Arial, Helvetica, sans-serif;
-			}
+				h1, p {
+					font-family: Arial, Helvetica, sans-serif;
+				}
 
-			h1 {
-				color: #515151;
-			}
+				h1 {
+					color: #515151;
+				}
 
-			nav a {
-				display: inline-block;
-				color: #FFF;
-				padding: 18px 12px;
-				text-decoration: none;
-				transition: ease-in .3s;
-			}
+				nav a {
+					display: inline-block;
+					color: #FFF;
+					padding: 18px 12px;
+					text-decoration: none;
+					transition: ease-in .3s;
+				}
 
-			nav a:hover {
-				color: #515151;
-				background: #FFF;
-			}
+				nav a:hover {
+					color: #515151;
+					background: #FFF;
+				}
 
-			nav ul {
-				list-style-type: none;
-			}
+				nav ul {
+					list-style-type: none;
+				}
 
-			nav ul li {
-				display: inline;
-			}
-		</style>
-    <body>
-		<img src="img/sysdig_Horz_Color_Logo_RGB_sml.jpg" alt="Sysdig Logo"/>
-		<nav>
-        <ul>
-				<li><a href="/">Home</a></li>
-            {{range .Links}}
-				<li><a href="/{{.}}">{{.}}</a></li>
-            {{end}}
-        </ul>
-		</nav>
-		<h1>{{.Title}}</h1>
-		<p>
-		`
+				nav ul li {
+					display: inline;
+				}
+			</style>
+		<body>
+			<img src="img/sysdig_Horz_Color_Logo_RGB_sml.jpg" alt="Sysdig Logo"/>
+			<nav>
+			<ul>
+					<li><a href="/">Home</a></li>
+				{{range .Links}}
+					<li><a href="/{{.}}">{{.}}</a></li>
+				{{end}}
+			</ul>
+			</nav>
+			<h1>{{.Title}}</h1>
+			<p>
+			`
+
+	w.Header().Add("Content Type", "text/html")
+	templates, _ := template.New("header").Parse(header)
+	templates.Lookup("header").Execute(w, context)
+
+}
+
+func writeHtmlFooter(w http.ResponseWriter, req *http.Request, context Context) {
 	const footer = `
 	</p>
     </body>
@@ -95,181 +88,58 @@ func hello(w http.ResponseWriter, req *http.Request) {
 `
 
 	w.Header().Add("Content Type", "text/html")
-	templates, _ := template.New("header").Parse(header)
-	context := Context{
-		Title: "Sysdig Hello World - Home",
+	templates, _ := template.New("footer").Parse(footer)
+	templates.Lookup("footer").Execute(w, context)
+
+}
+
+// =========
+
+func hello(w http.ResponseWriter, req *http.Request) {
+
+	c := Context{
+		Title: "Sysdig Hello World - Hello",
 		Links: [2]string{"headers", "hello"},
 	}
-	templates.Lookup("header").Execute(w, context)
-
+	writeHtmlHeader(w, req, c)
 	// data in the page - output html headers
 	// data in the page
 	fmt.Fprintf(w, "Hi there!\n")
 
-	templates2, _ := template.New("footer").Parse(footer)
-
-	templates2.Lookup("footer").Execute(w, nil)
+	writeHtmlFooter(w, req, c)
 }
 
 func defaultPage(w http.ResponseWriter, req *http.Request) {
 
-	const doc = `
-<!DOCTYPE html>
-<html>
-    <head>
-		<title>
-        {{.Title}}
-		</title>
-    </head>
-	<style>
-			nav {
-				background: #515151;
-				text-align: center;
-				font-family: Arial, Helvetica, sans-serif;
-				/* THIS IS OPTIONAL*/
-			}
-
-			h1, p {
-				font-family: Arial, Helvetica, sans-serif;
-			}
-
-			h1 {
-				color: #515151;
-			}
-
-			nav a {
-				display: inline-block;
-				color: #FFF;
-				padding: 18px 12px;
-				text-decoration: none;
-				transition: ease-in .3s;
-			}
-
-			nav a:hover {
-				color: #515151;
-				background: #FFF;
-			}
-
-			nav ul {
-				list-style-type: none;
-			}
-
-			nav ul li {
-				display: inline;
-			}
-		</style>
-    <body>
-		<img src="img/sysdig_Horz_Color_Logo_RGB_sml.jpg" alt="Sysdig Logo"/>
-		<nav>
-        <ul>
-				<li><a href="/">Home</a></li>
-            {{range .Links}}
-				<li><a href="/{{.}}">{{.}}</a></li>
-            {{end}}
-        </ul>
-		</nav>
-		<h1>{{.Title}}</h1>
-		<p>Please select your link.</p>
-    </body>
-</html>
-`
-
-	w.Header().Add("Content Type", "text/html")
-	templates, _ := template.New("doc").Parse(doc)
-	context := Context{
+	c := Context{
 		Title: "Sysdig Hello World - Home",
 		Links: [2]string{"headers", "hello"},
 	}
-	templates.Lookup("doc").Execute(w, context)
+	writeHtmlHeader(w, req, c)
+	// data in the page - output html headers
+	// data in the page
+	fmt.Fprintf(w, "Please select your link.\n")
+
+	writeHtmlFooter(w, req, c)
 
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {
 
-	const header = `
-<!DOCTYPE html>
-<html>
-    <head>
-		<title>
-        {{.Title}}
-		</title>
-    </head>
-	<style>
-			nav {
-				background: #515151;
-				text-align: center;
-				font-family: Arial, Helvetica, sans-serif;
-				/* THIS IS OPTIONAL*/
-			}
-
-			h1, p {
-				font-family: Arial, Helvetica, sans-serif;
-			}
-
-			h1 {
-				color: #515151;
-			}
-
-			nav a {
-				display: inline-block;
-				color: #FFF;
-				padding: 18px 12px;
-				text-decoration: none;
-				transition: ease-in .3s;
-			}
-
-			nav a:hover {
-				color: #515151;
-				background: #FFF;
-			}
-
-			nav ul {
-				list-style-type: none;
-			}
-
-			nav ul li {
-				display: inline;
-			}
-		</style>
-    <body>
-		<img src="img/sysdig_Horz_Color_Logo_RGB_sml.jpg" alt="Sysdig Logo"/>
-		<nav>
-        <ul>
-				<li><a href="/">Home</a></li>
-            {{range .Links}}
-				<li><a href="/{{.}}">{{.}}</a></li>
-            {{end}}
-        </ul>
-		</nav>
-		<h1>{{.Title}}</h1>
-		<p>
-		`
-	const footer = `
-	</p>
-    </body>
-</html>
-`
-
-	w.Header().Add("Content Type", "text/html")
-	templates, _ := template.New("header").Parse(header)
-	context := Context{
-		Title: "Sysdig Hello World - Home",
+	c := Context{
+		Title: "Sysdig Hello World - Headers",
 		Links: [2]string{"headers", "hello"},
 	}
-	templates.Lookup("header").Execute(w, context)
+	writeHtmlHeader(w, req, c)
 
 	// data in the page - output html headers
 	for name, headers := range req.Header {
 		for _, h := range headers {
-
 			fmt.Fprintf(w, "%v: %v\n", name, h)
 		}
 	}
 
-	templates2, _ := template.New("footer").Parse(footer)
-
-	templates2.Lookup("footer").Execute(w, nil)
-
+	writeHtmlFooter(w, req, c)
 }
 
 func main() {
